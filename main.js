9330bc8914b08afae05973d175808db6f13f5449
Shyrko1-21 <shyrko1.21@gmail.com>
2023-09-22 13:56:03 +0300

    commit

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('Hello from Electron')
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.webContents.openDevTools()
+
+  win.loadURL('http://192.168.100.4:3000/main-display/3')
+  // win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
